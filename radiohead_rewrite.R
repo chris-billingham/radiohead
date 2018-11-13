066f505234e97f92dab306850d554e8e92c02abe
Chris Billingham <chris.billingham@gmail.com>
2018-11-13 22:02:42 +0000

    ok i did a little bit of little mix

--- radiohead_rewrite.R
+++ radiohead_rewrite.R
@@ -137,3 +137,62 @@ radiohead_uni_sent %>%
 # so overall pretty gloomy since their first album
 
 # now, let's brighten things up a bit
+url <- "https://en.wikipedia.org/wiki/Little_Mix_discography#Studio_albums"
+
+# get the list of full studio albums for radiohead from wikipedia
+littlemix_albums <- read_html(url) %>% 
+  html_nodes("table.wikitable.plainrowheaders") %>%
+  html_table(fill = TRUE) %>%
+  .[[1]] %>%
+  slice(2:(n()-1)) %>%
+  .[,1] %>%
+  tolower()
+
+# let's use this amended function to create a single datafame
+littlemix_lyrics <- littlemix_albums %>%
+  map2_dfr("little mix", ., genius_album_plus)
+
+# we'll need this for later
+littlemix_albums <- data_frame(album = littlemix_albums) %>%
+  mutate(album_no = seq(1, length(littlemix_albums)))
+
+library(tidytext)
+
+littlemix_uni_sent <- littlemix_lyrics %>% 
+  unnest_tokens(word, lyric) %>% 
+  left_join(get_sentiments("afinn")) %>%
+  mutate(score = ifelse(is.na(score), 0, score)) %>%
+  left_join(littlemix_albums) %>%
+  group_by(album) %>%
+  mutate(pos = row_number()) %>% 
+  mutate(rollsum = cumsum(score))
+
+# initial visual on total album sentiment
+littlemix_uni_sent %>%
+  group_by(album_no, album) %>%
+  summarise(album_sentiment = sum(score)) %>%
+  ggplot(aes(fct_reorder(album, album_no), album_sentiment)) +
+  geom_col() +
+  ggtitle("Little Mix Sentiment per Album") +
+  xlab("Album name ordered by release date") +
+  ylab("Cumulative Sentiment per Album") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+
+# let's look at how the sentiment builds through the albums
+littlemix_uni_sent %>%
+  ggplot(aes(pos, rollsum)) + 
+  geom_line() + 
+  facet_wrap(~ fct_reorder(album, album_no), scales = "free") +
+  ggtitle("Little Mix Cumulative Sentiment per Album") +
+  xlab("Word Position in Album") +
+  ylab("Cumulative Sentiment per Album")
+
+# dna is definitely the off one out there
+littlemix_uni_sent %>% 
+  filter(album == "dna") %>%
+  ggplot(aes(pos, rollsum)) + 
+  geom_line() + 
+  facet_wrap(~ fct_reorder(track_title, track_n), scales = "free_x") +
+  ggtitle("DNA Cumulative Sentiment per Song") +
+  xlab("Word Position in Album") +
+  ylab("Cumulative Sentiment per Album")
